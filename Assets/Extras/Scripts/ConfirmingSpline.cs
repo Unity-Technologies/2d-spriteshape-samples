94235c25150f85a892bf858270c6634d8405bd8f
Venkatesh SC <venkatesh@unity3d.com>
2018-04-12 17:34:26 +0800

    [2d] Add some sample scripts posted on forum.

---
+++ Assets/Extras/Scripts/ConfirmingSpline.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+// Demo Script Usage:
+//   When you want multiple SpriteShapes to share a common Spline,
+//   attach this script to the secondary objects you would like to 
+//   copy the Spline and set the ParentObject to the original object
+//   you are copying from.
+
+[ExecuteInEditMode]
+public class ConfirmingSpline : MonoBehaviour
+{
+
+    public GameObject m_ParentObject;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_ParentObject != null)
+        {
+            CopySpline(m_ParentObject, gameObject);
+        }
+    }
+
+    private static void CopySpline(GameObject src, GameObject dst)
+    {
+#if UNITY_EDITOR
+        var parentSpriteShapeController = src.GetComponent<SpriteShapeController>();
+        var mirrorSpriteShapeController = dst.GetComponent<SpriteShapeController>();
+
+        if (parentSpriteShapeController != null && mirrorSpriteShapeController != null)
+        {
+            SerializedObject srcController = new SerializedObject(parentSpriteShapeController);
+            SerializedObject dstController = new SerializedObject(mirrorSpriteShapeController);
+            SerializedProperty srcSpline = srcController.FindProperty("m_Spline");
+            dstController.CopyFromSerializedProperty(srcSpline);
+            dstController.ApplyModifiedProperties();
+            EditorUtility.SetDirty(mirrorSpriteShapeController);
+        }
+#endif
+    }
+
+}
---
+++ Assets/Extras/Scripts/SimpleDraw.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+// Dynamic modification of spline to follow the path of mouse movement.
+// This script is just a simplified demo to demonstrate the idea.
+
+public class SimpleDraw : MonoBehaviour
+{
+    public float minimumDistance = 1.0f;
+    private Vector3 lastPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+    
+	// Update is called once per frame
+	void Update ()
+    {
+        var mp = Input.mousePosition;
+        mp.z = 10.0f;
+        mp = Camera.main.ScreenToWorldPoint(mp);
+        var dt = Mathf.Abs((mp - lastPosition).magnitude);
+        var md = (minimumDistance > 1.0f) ? minimumDistance : 1.0f;
+        if (Input.GetMouseButton(0) && dt > md)
+        {
+            var spriteShapeController = gameObject.GetComponent<SpriteShapeController>();
+            var spline = spriteShapeController.spline;
+            spline.InsertPointAt(spline.GetPointCount(), mp);
+            var newPointIndex = spline.GetPointCount() - 1;
+            spline.SetTangentMode(newPointIndex, ShapeTangentMode.Linear);
+
+            // BevelCutoff and BevelSize can be used when the point and its neighbor
+            // points on both sudes are linear. This creates a nice automatic curve.
+            spline.SetBevelCutoff(newPointIndex, 150.0f);
+            spline.SetBevelSize(newPointIndex, 0.25f);
+            spline.SetHeight(newPointIndex, 1.0f);
+            lastPosition = mp;
+            Debug.Log(dt);
+        }
+	}
+}
